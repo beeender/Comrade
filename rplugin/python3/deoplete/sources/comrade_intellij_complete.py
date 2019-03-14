5bb5ea6c9be6dbbf831dfd34aa9e45c543207e0e
Chen Mulong <chenmulong@gmail.com>
2019-03-14 15:06:09 +0800

    Fix python warnings

--- rplugin/python3/deoplete/sources/comrade_intellij_complete.py
+++ rplugin/python3/deoplete/sources/comrade_intellij_complete.py
@@ -1,21 +1,21 @@
-import re
 from .base import Base
-from deoplete.util import getlines
+
 
 class Source(Base):
     def __init__(self, vim):
         Base.__init__(self, vim)
 
-        self.name = 'ComradeIntelliJ-complete'
+        self.name = 'Comrade'
         self.mark = '[Cde]'
         self.filetypes = []
-        self.rank = 150
+        self.rank = 200
         self.max_pattern_length = 100
-        self.input_pattern = '[^. \t0-9]\.\w*'
+        self.min_pattern_length = 1
+        # self.input_pattern = '^\\s*\\S+'
+        self.input_pattern = '[^. \t0-9]\\.\\w*'
         self.is_debug_enabled = True
 
     def gather_candidates(self, context):
-        #self.print_error(context)
         buf_id = context["bufnr"]
         buf_changedtick = self.vim.request("nvim_buf_get_changedtick", buf_id)
         buf_name = self.vim.request("nvim_buf_get_name", buf_id)
@@ -24,12 +24,12 @@ class Source(Base):
         row = win.cursor[0] - 1
         col = win.cursor[1]
         ret = {
-            "buf_id" : buf_id,
-            "buf_name" : buf_name,
-            "buf_changedtick" : buf_changedtick,
-            "row" : row,
-            "col" : col,
-            "new_request" : not context["is_async"]};
+            "buf_id": buf_id,
+            "buf_name": buf_name,
+            "buf_changedtick": buf_changedtick,
+            "row": row,
+            "col": col,
+            "new_request": not context["is_async"]}
         results = self.vim.call("comrade#RequestCompletion", buf_id, ret)
 
         if results:
